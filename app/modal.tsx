78db515336ef656e72512ab698b289b4c6f7ca7a
kaanguru <kaanguru@mail.ru>
2024-12-08 21:55:14 +0300

    powered started

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Platform } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { Text } from 'react-native';
 
 export default function Modal() {
   return (
     <>
-      <ScreenContent path="app/modal.tsx" title="Modal" />
+      <Text>Modal</Text>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </>
   );
